d4aa9546f3c671bb2ee91d7e2d6d31915f48b21d
Vs-kb-07 <138425449+Vs-kb-07@users.noreply.github.com>
2024-05-07 23:56:34 +0530

    Add files via upload

---
+++ 5th_anova.py
@@ -0,0 +1,25 @@
+ANOVA (Analysis of Variance) 
+Perform one-way ANOVA to compare means across multiple groups. Conduct post-hoc tests to identify significant differences between group means
+
+import pandas as pd
+import scipy.stats as stats
+from statsmodels.stats.multicomp import pairwise_tukeyhsd
+
+group1 = [23, 25, 29, 34, 30]
+group2 = [19, 20, 22, 24, 25]
+group3 = [15, 18, 20, 21, 17]
+group4 = [28, 24, 26, 30, 29]
+
+all_data = group1 + group2 + group3 + group4
+group_labels = ['Group1'] * len(group1) + ['Group2'] * len(group2) + ['Group3'] * len(group3) + ['Group4'] * len(group4)
+
+(keep all group_labels in one line otherwise it will show error)
+
+f_statistics, p_value = stats.f_oneway(group1, group2, group3, group4)
+print("one-way ANOVA:")
+print("F-statistics:", f_statistics)
+print("p-value", p_value)
+tukey_results = pairwise_tukeyhsd(all_data, group_labels)
+print("\nTukey-Kramer post-hoc test:")
+print(tukey_results)
+
