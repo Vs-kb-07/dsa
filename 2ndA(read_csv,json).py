8e3fed08f41637cd5be32ee19314907b3050e2eb
Vs-kb-07 <138425449+Vs-kb-07@users.noreply.github.com>
2024-05-07 20:24:42 +0530

    Add files via upload

---
+++ 2ndA(read_csv,json).py
@@ -0,0 +1,19 @@
+A.  1)	#Read data from a csv file
+import pandas as pd
+df = pd.read_csv('D:\data science\student-scores.csv')
+print("Our dataset ")
+print(df)
+
+
+   2)	# Reading data from a JSON file
+import pandas as pd 
+data = pd.read_json(' D:\\data science\\ sales.json')
+print(data)
+
+
+
+
+
+
+
+
